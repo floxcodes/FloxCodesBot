54913a9635834c68c8548bbae0feb38e33b64a45
floxcodes <GamerRDaio@outlook.com>
2025-04-05 16:10:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-()
+import discord
+from discord.ext import commands
+import yaml
+
+with open('config.yml') as f:
+    config = yaml.safe_load(f)
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
+bot.run(config['token'])
